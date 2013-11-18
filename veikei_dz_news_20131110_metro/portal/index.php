1e738fc5d2bf7ebf5b8a16e4159f32b6e19b4920
root <root@www.xuanhaoke.com>
2013-11-18 12:34:28 +0800

    first

---
+++ veikei_dz_news_20131110_metro/portal/index.php
@@ -0,0 +1,341 @@
+<?php exit; ?>
+<!--{subtemplate common/header}-->
+<style id="diy_style" type="text/css"></style>
+
+
+
+
+
+<!--  上部广告图片占位   -->
+<div class="wp vk_index">
+  		<!--[diy=diy_vk_ad_top]--> <div id="diy_vk_ad_top" class="area"></div><!--[/diy]--> 
+</div>
+
+
+
+
+<!--  上部图片   -->
+<div class="wp vk_index">
+  		<!--[diy=diy_col_1-3]--> <div id="diy_col_1-3" class="area"></div><!--[/diy]--> 
+</div>
+
+
+
+
+
+
+
+
+
+
+<div class="wp col col_2-1  col_vk_title ">
+    
+    <!--幻灯片模块--> 
+    <div class="col_2-1_l">
+      <div class="vk_portal_slide_top vk_float"  style="height:350px; width:645px; float:left; clear:both;"> 
+      <!--[diy=diy_vk_portal_slide_top]-->  <div id="diy_vk_portal_slide_top" class="area"> </div>  <!--[/diy]-->
+      </div>
+
+      <div class="vk_portal_info_left vk_float" > <!--[diy=diy_vk_portal_info_left_2]--> <div id="diy_vk_portal_info_left_2" class="area"></div>  <!--[/diy]--></div>
+    </div>
+    <!--幻灯片模块--> 
+
+
+    <!-- 上部右侧模块 start-->
+    <div class="col_2-1_r  col_vk_title ">
+
+    	<div class=" "><!--[diy=diy_vk_top_r_1]-->   <div id="diy_vk_top_r_1" class="area"></div>  <!--[/diy]--></div>
+
+    
+      	<div class=" "> <!--[diy=diy_vk_portal_top_sd]--> <div id="diy_vk_portal_top_sd" class="area"></div>  <!--[/diy]--></div>
+
+        
+    </div>
+    <!-- 上部右侧模块 end -->
+
+</div>
+
+
+
+
+<div class="wp col col_2-1  col_vk_title ">
+      
+    <div class="col_2-1_l">
+      <div class="vk_portal_info_left vk_float" > <!--[diy=diy_vk_portal_info_left]--> <div id="diy_vk_portal_info_left" class="area"></div>  <!--[/diy]--></div>
+    </div>
+
+    <div class="col_2-1_r  col_vk_title ">
+    	<div class="vk_top_post_pic_edit"> <a class=" " href="#" >  </a> </div>
+    </div>
+</div>
+
+
+  
+
+<div class="wp vk_index vk_week_hot">
+  		<!--[diy=diy_vk_week_hot]--> <div id="diy_vk_week_hot" class="area"></div><!--[/diy]--> 
+</div>
+
+
+
+ 
+
+<!--  焦点下部 全屏横幅广告占位   -->
+<div class="wp vk_index">
+  		<!--[diy=diy_vk_ad_001]--> <div id="diy_vk_ad_001" class="area"></div><!--[/diy]--> 
+</div>
+
+
+<!--  图文模块 I   -->
+<div class="wp vk_index col_inner bg_vk_title_h2_blue_dark">
+    <h2>
+    	<a href="#" target="_blank"> 学前亲子 <strong>   </strong> </a>  
+        <span>
+        <a href="#" > 新闻 </a> <em> | </em>
+        <a href="#" > 娱乐 </a> <em> | </em>
+        <a href="#" > 手机 </a> <em> | </em>
+        <a href="#" > 平板 </a> <em> | </em>
+        <a href="#" > 计算机 </a> <em> | </em>
+        <a href="#" > 数码 </a> <em> | </em>
+        <a href="#" > 硬件 </a> <em> | </em>
+        <a href="#" > 更多 + </a> <em>  </em>
+        </span>
+    </h2>
+	<div class="col_inner_ct">
+        <div class="col_2-3-1_l">
+            <!--[diy=diy_vk_news_1_1]--> <div id="diy_vk_news_1_1" class="area"></div><!--[/diy]--> 
+        </div> 
+        
+        <div class="col_2-3-1_m ">
+            <!--[diy=diy_vk_news_1_2]--> <div id="diy_vk_news_1_2" class="area"></div><!--[/diy]--> 
+        </div>
+    
+        <div class="col_2-3-1_r ">
+            <!--[diy=diy_vk_news_1_r]--> <div id="diy_vk_news_1_r" class="area"></div><!--[/diy]--> 
+        </div> 
+    </div>
+</div>
+
+  
+  
+
+<!--  焦点下部 全屏横幅广告占位   -->
+<div class="wp vk_index">
+  		<!--[diy=diy_vk_ad_2]--> <div id="diy_vk_ad_2" class="area"></div><!--[/diy]--> 
+</div>
+
+  
+
+<!--  首页 全屏横幅广告占位 3  -->
+<div class="wp vk_index">
+  		<!--[diy=diy_vk_ad_3]--> <div id="diy_vk_ad_3" class="area"></div><!--[/diy]--> 
+</div>
+
+
+<!--  导读：社区模块   -->
+
+<div class="wp vk_index col_inner bg_vk_title_h2_green">
+
+    <h2>
+    	<a href="#" target="_blank"> 生活资讯 <strong>  Life </strong>  </a>  
+        <span>
+        <a href="#" > 娱乐 </a> <em> | </em>
+        <a href="#" > 明星 </a> <em> | </em>
+        <a href="#" > 科技 </a> <em> | </em>
+        <a href="#" > 生活 </a> <em> | </em>
+        <a href="#" > 数码 </a> <em> | </em>
+        <a href="#" > 更多 + </a> <em>  </em>
+        </span>
+    </h2>
+
+	<div class="col_inner_ct">
+        <div class="col_2-3-1_l">
+            <!--[diy=diy_vk_guide_1_1]--> <div id="diy_vk_guide_1_1" class="area"></div><!--[/diy]--> 
+        </div> 
+        
+        <div class="col_2-3-1_m ">
+            <!--[diy=diy_vk_guide_1_2]--> <div id="diy_vk_guide_1_2" class="area"></div><!--[/diy]--> 
+        </div>
+    
+        <div class="col_2-3-1_r ">
+            <!--[diy=diy_vk_guide_1_r]--> <div id="diy_vk_guide_1_r" class="area"></div><!--[/diy]--> 
+        </div> 
+    </div>
+</div>
+
+  
+
+<!--  首页 全屏横幅广告占位 4  -->
+<div class="wp vk_index">
+  		<!--[diy=diy_vk_ad_4]--> <div id="diy_vk_ad_4" class="area"></div><!--[/diy]--> 
+</div>
+
+
+<div class="wp col_inner col_2-3-1  col_vk_title bg_vk_title_h2_blue">
+
+    <h2>
+    	<a href="#" target="_blank"> 数码资讯<strong>  Phone </strong>  </a>  
+        <span>
+        <a href="#" > iPhone </a> <em> | </em>
+        <a href="#" > 三星 </a> <em> | </em>
+        <a href="#" > 华为 </a> <em> | </em>
+        <a href="#" > 谷歌 </a> <em> | </em>
+        <a href="#" > 国产手机 </a> <em> | </em>
+        <a href="#" > 3G </a> <em> | </em>
+        <a href="#" > 更多 + </a> <em>  </em>
+        </span>
+    </h2>
+
+	<div class="col_inner_ct">
+    <div class="col_2-3-1_l">
+  		<!--[diy=diy_vk_guide_1_1_2]--> <div id="diy_vk_guide_1_1_2" class="area"></div><!--[/diy]--> 
+    </div> 
+    
+    <div class="col_2-3-1_m ">
+  		<!--[diy=diy_vk_guide_1_2_2]--> <div id="diy_vk_guide_1_2_2" class="area"></div><!--[/diy]--> 
+    </div>
+
+    <div class="col_2-3-1_r ">
+  		<!--[diy=diy_vk_guide_1_r_2]--> <div id="diy_vk_guide_1_r_2" class="area"></div><!--[/diy]--> 
+    </div> 
+    </div>
+</div>
+
+  
+
+<!--  首页 全屏横幅广告占位 4  -->
+<div class="wp vk_index">
+  		<!--[diy=diy_vk_ad_4_1]--> <div id="diy_vk_ad_4_1" class="area"></div><!--[/diy]--> 
+</div>
+
+
+
+
+<div class="wp col_inner  col_vk_title bg_vk_title_h2_orange">
+
+    <h2>
+    	<a href="#" target="_blank"> 壁纸/主题 <strong> Wallpaper </strong> </a>  
+        <span>
+        <a href="#" > 手机壁纸 </a> <em> | </em>
+        <a href="#" > 平板壁纸 </a> <em> | </em>
+        <a href="#" > 桌面壁纸 </a> <em> | </em>
+        <a href="#" > 美女壁纸 </a> <em> | </em>
+        <a href="#" > 更多 + </a> <em>  </em>
+        </span>
+    </h2>
+
+	<div class="col_inner_ct">
+	<div  class="col_3-1_l">
+  		<!--[diy=diy_vk_guide_2_2_1]--> <div id="diy_vk_guide_2_2_1" class="area"></div><!--[/diy]--> 
+    </div> 
+	<div  class="col_3-1_r vk_tab m_r_0 right">
+  		<!--[diy=diy_vk_guide_2_1_1]--> <div id="diy_vk_guide_2_1_1" class="area"></div><!--[/diy]--> 
+    </div> 
+    </div>
+</div>
+
+
+
+  
+  
+  
+
+<!--  首页 全屏横幅广告占位 5  -->
+<div class="wp vk_index">
+  		<!--[diy=diy_vk_ad_5]--> <div id="diy_vk_ad_5" class="area"></div><!--[/diy]--> 
+</div>
+
+
+
+<!--  图文模块 II - 可用作网友相册或第二个图文模块   -->
+<div class="wp vk_index col_inner bg_vk_title_h2_red">
+    <h2>
+    <a href="#" target="_blank"> 网友秀图 <strong>  Show </strong> </a> 
+    <span> <a href="#" > 更多 + </a> <em>  </em> </span>
+    </h2>
+	<div class=" ">
+  		<!--[diy=diy_vk_pic_2_1]--> <div id="diy_vk_pic_2_1" class="area"></div><!--[/diy]-->
+    </div> 
+</div>
+
+
+
+<!--  首页 全屏横幅广告占位 4  -->
+<div class="wp vk_index">
+  		<!--[diy=diy_vk_ad_4_2]--> <div id="diy_vk_ad_4_2" class="area"></div><!--[/diy]--> 
+</div>
+
+
+
+
+<!--  导读：消费模块  -->
+<div class="wp vk_index col_inner  col_vk_title bg_vk_title_h2_blue_dark">
+    <h2>
+    	<a href="#" target="_blank"> 社区动态 <strong>  Forum </strong> </a>  
+        <span>
+        <a href="#" > 资讯 </a> <em> | </em>
+        <a href="#" > 二手 </a> <em> | </em>
+        <a href="#" > 话题 </a> <em> | </em>
+        <a href="#" > 教程 </a> <em> | </em>
+        <a href="#" > 热议 </a> <em> | </em>
+		<a class="vk_fast_post" href="forum.php?mod=misc&action=nav" onclick="showWindow('nav', this.href, 'get', 0)"> 快捷发布 + </a>
+        </span>
+    </h2>
+
+	<div class="col_inner_ct">
+	<div  class="col_3-1_l">
+  		<!--[diy=diy_vk_guide_2_2]--> <div id="diy_vk_guide_2_2" class="area"></div><!--[/diy]--> 
+    </div> 
+	<div  class="col_3-1_r vk_tab m_r_0 right">
+  		<!--[diy=diy_vk_guide_2_1]--> <div id="diy_vk_guide_2_1" class="area"></div><!--[/diy]--> 
+    </div>
+	</div>
+</div>
+  
+
+<!--  首页 全屏横幅广告占位 5  -->
+<div class="wp vk_index">
+  		<!--[diy=diy_vk_ad_6]--> <div id="diy_vk_ad_6" class="area"></div><!--[/diy]--> 
+</div>
+
+
+
+<!--  图文模块 I   -->
+<div class="wp vk_index col_inner  vk_pic_ie6 bg_vk_title_h2_red">
+    <h2>
+    	<a href="#" target="_blank"> 时尚美图 <strong> Pictures </strong> </a>  
+        <span>
+        <a href="#" > 美女画报 </a> <em> | </em>
+        <a href="#" > 手机摄影 </a> <em> | </em>
+        <a href="#" > 壁纸下载 </a> <em> | </em>
+        <a href="#" > 手机主题 </a> <em> | </em>
+        <a href="#" > 更多 + </a> <em>  </em>
+        </span>
+    </h2>
+	<div class=" ">
+		<!--[diy=diy_vk_pic_1_1]--> <div id="diy_vk_pic_1_1" class="area"></div><!--[/diy]--> 
+    </div> 
+</div>
+
+
+
+
+<div class="wp vk_index">  
+  <!--[diy=diy_ad_bottom_1]--> <div id="diy_ad_bottom_1" class="area"></div><!--[/diy]--> 
+</div>
+
+<div class="wp col_inner vk_index ">
+	<!--
+        <h2><a href="#" target="_blank"> 友情链接 </a> <span><a href="#"> 申请链接 +  </a></span></h2>
+	-->
+	<div class="col_inner_ct">
+  		<!--[diy=diy_ad_2]--> <div id="diy_ad_2" class="area"></div><!--[/diy]--> 
+  	</div>
+</div>
+
+
+<div  style="clear:both;"></div>
+
+
+<script src="misc.php?mod=diyhelp&action=get&type=index&diy=$_G['gp_diy']&r={echo random(4)}" type="text/javascript"></script> 
+<!--{subtemplate common/footer}--> 
